8628668cf24331b493f36e78e4446e1214840315
Gastón Sobrevilla <gastonsobrevilla@gmail.com>
2020-09-26 15:22:05 -0300

    Configured initial view controller and root navigation vc

---
+++ TheMeal/ViewControllers/InitialViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InitialViewController.swift
+//  TheMeal
+//
+//  Created by Gastón Sobrevilla on 26/09/2020.
+//  Copyright © 2020 Gastón Sobrevilla. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+}
+
